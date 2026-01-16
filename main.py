3babd1861d5bf16f9372fbf09ca95da8be331ef6
akumushbibi2197 <akumushbibi2197@gmail.com>
2026-01-16 21:37:14 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+#1-misol
+ism = input("Ismingizni kiriting: ")
+print("Salom,", ism)
+
+#2-misol
+a = int(input("1-sonni kiriting: "))
+b = int(input("2-sonni kiriting: "))
+print("Yig‘indi:", a + b)
+
+#3-misol
+a = int(input("Uzunlikni kiriting: "))
+b = int(input("Enini kiriting: "))
+
+perimetr = 2 * (a + b)
+yuza = a * b
+
+print("Perimetr:", perimetr)
+print("Yuza:", yuza)
+
+#4-misol
+r = float(input("Radiusni kiriting: "))
+pi = 3.14
+
+uzunlik = 2 * pi * r
+print("Aylana uzunligi:", uzunlik)
+
+#5-misol
+a = int(input("1-son: "))
+b = int(input("2-son: "))
+c = int(input("3-son: "))
+
+orta = (a + b + c) / 3
+print("O‘rtacha qiymat:", orta)
+
